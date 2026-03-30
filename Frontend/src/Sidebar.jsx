8ca8d93de52c7cd78425f371b36832f7a155fdec
Dhruvsingh121 <GodZeusOP@gmail.com>
2026-03-30 18:19:59 +0530

    Fixed Bugs

--- Frontend/src/Sidebar.jsx
+++ Frontend/src/Sidebar.jsx
@@ -62,7 +62,7 @@ function Sidebar() {
   const deleteThread = async (threadId) => {
     try {
       const response = await fetch(
-        `http://localhost:8080/api/thread/${threadId}`,
+        `https://dev-mind.onrender.com/api/thread/${threadId}`,
         { method: "DELETE" },
       );
       const res = await response.json();
